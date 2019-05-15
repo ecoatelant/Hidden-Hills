194f4c6ae52b3e52bb399b8e0164005c3f647926
YanisSellali.IUTMontreuil <49198033+BrayanBoss@users.noreply.github.com>
2019-05-15 11:43:08 +0200

    Affichage personnage et map à faire

---
+++ Hidden-Hills-Dev/Hidden Hills/src/controleur/ControleurMap.java
@@ -0,0 +1,101 @@
+package controleur;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
+import modeles.Personnage;
+import modeles.Map;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class ControleurMap implements Initializable {
+
+    private Map map;
+    
+    @FXML
+    private Pane mainPane;
+
+    @FXML
+    private TilePane tilePaneMap;
+
+    @FXML
+  	private Pane persoPane;
+
+    private Personnage p = new Personnage();
+  
+    boolean running, goNorth, goSouth, goEast, goWest;
+
+  	@FXML
+  	private ImageView imgVi;
+
+  	public void handlePressed(KeyEvent e) {
+  		switch (e.getCode()) {
+          	case Z:  
+          		goNorth = true; 
+          		handle('N');
+          		break;
+          	case S:  
+          		goSouth = true;
+          		handle('S');
+          		break;
+          	case Q:  
+          		goWest  = true;         		
+          		handle('W');
+          		break;
+          	case D:  
+          		goEast  = true;
+          		handle('E');
+          		break;
+          	default:
+          		break;
+  		}
+  	}
+  	
+  	public void handleReleased(KeyEvent e) {
+  		switch (e.getCode()) {
+  			case Z:  goNorth = false; break;
+  			case S:  goSouth = false; break;
+          	case Q:  goWest  = false; break;
+          	case D:  goEast  = false; break;
+          	default: break;
+  		}
+  	}
+      
+      public void handle(char direction) {
+          int dx = 0, dy = 0;
+
+          if (direction=='N') dy -= 5;
+          if (direction=='S') dy += 5;
+          if (direction=='E') dx += 5;
+          if (direction=='W') dx -= 5;
+
+          p.move(dx, dy);
+      }
+      
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		map = new Map();
+        for(int i = 0; i < this.map.getMap().size(); i++) {
+            Image img = new Image (this.map.getBlock(i).getuRI());
+			ImageView imgV = new ImageView (img);
+			imgV.setFitHeight(32);
+			imgV.setFitWidth(32);
+			imgV.setTranslateX(0);
+			imgV.setTranslateY(-500);
+			tilePaneMap.getChildren().add(imgV);
+        }
+        
+        imgVi = new ImageView ("file:src/image/persoMod.png");
+		imgVi.translateXProperty().bind(this.p.xProperty());
+		imgVi.translateYProperty().bind(this.p.yProperty());
+		imgVi.setFocusTraversable(true);
+		persoPane.getChildren().add(imgVi);
+		persoPane.requestFocus();		
+		persoPane.setOnKeyPressed(e -> handlePressed(e));
+		persoPane.setOnKeyReleased(e -> handleReleased(e));
+	}
+}
---
+++ Hidden-Hills-Dev/Hidden Hills/src/modeles/Map.java
@@ -0,0 +1,70 @@
+package modeles;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import modeles.Block;
+
+public class Map {
+	
+	private ObservableList<Block> map;
+	 
+	public Map () {
+		this.map = FXCollections.observableArrayList();
+		BufferedReader file;
+		try {
+			file = new BufferedReader(new FileReader("Hidden Hills/src/Map.csv"));
+			try {
+				while (file.ready()) {
+					try {
+						//On récupère l'id des blocks en les découpants à partir des virgules 
+						String[] parts= file.readLine().split(",");
+						for(int i=0; i<parts.length;i++) {
+						this.map.add(new Block(parts[i]));
+						}
+					}catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e1) {
+				System.out.println("Fichier introuvable");
+				e1.printStackTrace();
+		}
+	}	
+	
+	public void sauvegarderMap() {
+        try {
+            File file = new File("Hidden Hills/src/vue/Map.csv");
+            FileWriter fileWriter = new FileWriter(file, false);
+            String changements = "";
+            int i = 1;
+            for (Block t : map) {
+                changements = changements + t.getId()+",";
+                if (i%this.map.size() ==0)
+                    changements = changements + "\n";
+                i++;
+            }
+            fileWriter.write(changements);
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	public Block getBlock (int indice) {
+	        return this.map.get(indice);
+	}
+	
+	public ObservableList<Block> getMap() {
+	        return this.map;
+	}
+
+
+}
